30a255bb7b00363752e46ab0a84df2ea22e21f64
blackdragon2447 <blackdragon2447@e.email>
2022-09-11 14:55:29 +0200

    Remove features used for the old filehandler and update workflow

--- actions.sh
+++ actions.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 cargo +nightly build --verbose
-cargo +nightly build --no-default-features --features ron-config --verbose
 cargo clippy --all-targets
-cargo clippy --no-default-features --features ron-config --all-targets
 cargo fmt -- --check
