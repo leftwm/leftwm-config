b27905d02e0d73697239ad0d34f0a66890badd02
BlackDragon2447 <blackdragon2447@e.email>
2023-08-04 15:35:18 +0200

    Remove commented code

--- src/config/filehandler.rs
+++ src/config/filehandler.rs
@@ -1,31 +1,3 @@
-/*
-#[cfg(feature = "guess-config")]
-mod guess;
-#[cfg(feature = "guess-config")]
-pub use guess::generate_new_config;
-#[cfg(feature = "guess-config")]
-pub use guess::get_config_file;
-#[cfg(feature = "guess-config")]
-pub use guess::load;
-#[cfg(feature = "guess-config")]
-pub use guess::load_from_file;
-#[cfg(feature = "guess-config")]
-pub use guess::save_to_file;
-
-#[cfg(feature = "ron-config")]
-mod ron;
-#[cfg(feature = "ron-config")]
-pub use self::ron::generate_new_config;
-#[cfg(feature = "ron-config")]
-pub use self::ron::get_config_file;
-#[cfg(feature = "ron-config")]
-pub use self::ron::load;
-#[cfg(feature = "ron-config")]
-pub use self::ron::load_from_file;
-#[cfg(feature = "ron-config")]
-pub use self::ron::save_to_file;
- */
-
 use std::fs;
 use std::fs::File;
 use std::io::Write;
